6c633cbe1906f47ed0c9f01746caac4189bacb10
Santiago Monteiro <santiago.a.monteiro@uea.edu.br>
2024-06-03 14:23:39 -0400

    get day of year

---
+++ src/utils/get-day-of-year.ts
@@ -0,0 +1,20 @@
+function isLeapYear(year: number) {
+  return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)
+}
+
+function getDayOfYear(date: Date) {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  const daysInMonth = [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  let dayOfYear = 0
+
+  for (let i = 0; i < month - 1; i++) {
+    dayOfYear += daysInMonth[i]
+  }
+
+  dayOfYear += day
+
+  return dayOfYear
+}
